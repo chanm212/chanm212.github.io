af2fca59eab54ac6098219a09c08943fbffc9478
chanm212 <chanm212@newschool.edu>
2020-03-04 12:06:24 -0500

    push zine onto website

---
+++ zine-final/assets/css/main.js
@@ -0,0 +1,106 @@
+console.log("hello")
+
+function printMousePos(event) {
+  var y = event.pageY
+}
+document.addEventListener("click", printMousePos);
+
+function robin() {
+  var x = document.querySelector(".robin");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY-100+"px";
+}
+
+function parsons() {
+  var x = document.querySelector(".parsons");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY-100+"px";
+}
+
+function ballet() {
+  var x = document.querySelector(".ballet");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY+400+"px";
+}
+
+function alexander() {
+  var x = document.querySelector(".alexander");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY-300+"px";
+}
+
+function met() {
+  var x = document.querySelector(".met");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY-100+"px";
+}
+
+function lynne() {
+  var x = document.querySelector(".lynne");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY+270+"px";
+}
+
+function historical() {
+  var x = document.querySelector(".historical");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY+400+"px";
+}
+
+function digital() {
+  var x = document.querySelector(".digital");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY-100+"px";
+}
+
+function kindle() {
+  var x = document.querySelector(".kindle");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY+700+"px";
+}
+
+function mother() {
+  var x = document.querySelector(".mother");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  x.style.top = event.pageY-140+"px";
+}
