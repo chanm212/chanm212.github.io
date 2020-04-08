5151cfa640be3f6244aee85a4bf9d5aecf12c6e3
chanm212 <chanm212@newschool.edu>
2020-04-08 13:38:08 -0500

    add jquery to homepage and restructure homepage

---
+++ homepagemain.js
@@ -0,0 +1,117 @@
+$(function() {
+  // Your jQuery code goes inside this special $(function) call
+  
+  console.log("i hope this works");
+  
+  $("#aniwords").on('click', function() {
+    
+    console.log("clicked");
+    $("#aniimages").css(
+      {zIndex:"1"});
+    $("#aniwords").css(
+      {opacity:"0"});
+
+  });
+
+  $("#zinewords").on('click', function() {
+    
+    console.log("clicked");
+    $("#zineimages").css(
+      {zIndex:"1"});
+    $("#zinewords").css(
+      {opacity:"0"});
+
+  });
+
+  $("#eventwords").on('click', function() {
+    
+    console.log("clicked");
+    $("#eventimages").css(
+      {zIndex:"1"});
+    $("#eventwords").css(
+      {opacity:"0"});
+
+  });
+
+  $("#networds").on('click', function() {
+    
+    console.log("clicked");
+    $("#netimages").css(
+      {zIndex:"1"});
+    $("#networds").css(
+      {opacity:"0"});
+
+  });
+
+  $("#geo3words").on('click', function() {
+    
+    console.log("clicked");
+    $("#geo3images").css(
+      {zIndex:"1"});
+    $("#geo3words").css(
+      {opacity:"0"});
+
+  });
+
+    $("#geo2words").on('click', function() {
+    
+    console.log("clicked");
+    $("#geo2images").css(
+      {zIndex:"1"});
+    $("#geo2words").css(
+      {opacity:"0"});
+
+  });
+
+      $("#geo1words").on('click', function() {
+    
+    console.log("clicked");
+    $("#geo1images").css(
+      {zIndex:"1"});
+    $("#geo1words").css(
+      {opacity:"0"});
+
+  });
+
+      $("#timewords").on('click', function() {
+    
+    console.log("clicked");
+    $("#timeimages").css(
+      {zIndex:"1"});
+    $("#timewords").css(
+      {opacity:"0"});
+
+  });
+
+      $("#archivewords").on('click', function() {
+    
+    console.log("clicked");
+    $("#archiveimages").css(
+      {zIndex:"1"});
+    $("#archivewords").css(
+      {opacity:"0"});
+
+  });
+
+      $("#codingwords").on('click', function() {
+    
+    console.log("clicked");
+    $("#codingimages").css(
+      {zIndex:"1"});
+    $("#codingwords").css(
+      {opacity:"0"});
+
+  });
+
+      $("#surveywords").on('click', function() {
+    
+    console.log("clicked");
+    $("#surveyimages").css(
+      {zIndex:"1"});
+    $("#surveywords").css(
+      {opacity:"0"});
+
+  });
+
+});
+  
