53e811920c957e93e1d10e83cbb45c4036c8ff92
chanm212 <chanm212@newschool.edu>
2020-04-18 13:48:37 -0500

    push final isolation diary and update homepage

--- isolation-diary/assets/javascript/main2.js
+++ isolation-diary/assets/javascript/main2.js
@@ -1,21 +1,94 @@
 $(document).ready(function() {
 
   console.log("hello");
-  $(window).scrollTop();
-
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 1200 ) {
-      $(".sixteen").css({position: "fixed"});
-    }
-    else {
-      $(".sixteen").css({position: "absolute" });
-    }
-
+  $(".scroll-to-top").click(function() {
+    $("html, body").animate({scrollTop: 0}, 0);
+    return false;
   });
 
+  if ($(window).width() <= 768 ) {
 
+      $("#picture15").click(function() {
+      $("#picture15").css({display:"none"});
+      $("#text15").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
 
+    $("#picture16").click(function() {
+      $("#picture16").css({display:"none"});
+      $("#text16").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
 
+    $("#picture17").click(function() {
+      $("#picture17").css({display:"none"});
+      $("#text17").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture18").click(function() {
+      $("#picture18").css({display:"none"});
+      $("#text18").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture19").click(function() {
+      $("#picture19").css({display:"none"});
+      $("#text19").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture20").click(function() {
+      $("#picture20").css({display:"none"});
+      $("#text20").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture21").click(function() {
+      $("#picture21").css({display:"none"});
+      $("#text21").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture22").click(function() {
+      $("#picture22").css({display:"none"});
+      $("#text22").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture23").click(function() {
+      $("#picture23").css({display:"none"});
+      $("#text23").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture24").click(function() {
+      $("#picture24").css({display:"none"});
+      $("#text24").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture25").click(function() {
+      $("#picture25").css({display:"none"});
+      $("#text25").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture26").click(function() {
+      $("#picture26").css({display:"none"});
+      $("#text26").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture27").click(function() {
+      $("#picture27").css({display:"none"});
+      $("#text27").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture28").click(function() {
+      $("#picture28").css({display:"none"});
+      $("#text28").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture29").click(function() {
+      $("#picture29").css({display:"none"});
+      $("#text29").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    $("#picture30").click(function() {
+      $("#picture30").css({display:"none"});
+      $("#text30").css({display:"grid",height:"300px", paddingTop:"100px"});
+    });
+
+    };
 
 
 });
